35d4abc0429f3cf442ea78e2b824dda504b3c733
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-10-02 03:21:00 +0600

    root api created

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send(" Working perfectly");
+});
+
 const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hia2w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
